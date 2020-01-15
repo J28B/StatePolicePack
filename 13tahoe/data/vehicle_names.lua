c4370bc91ebe7ea4b0a9ca60843809942d56ccf5
Jamie <jamiebeggs28@gmail.com>
2020-01-15 22:08:04 +0000

    Inital

---
+++ 13tahoe/data/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	--AddTextEntry("13tahoe", "13tahoe")
+end)
