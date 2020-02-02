20a2a9b397e8dcf434c509847e39bb59821cbe8f
Jamie <jamiebeggs28@gmail.com>
2020-02-02 19:29:03 +0000

    Added new working FPIS.

--- FORD FPIS/__resource.lua
+++ FORD FPIS/__resource.lua
@@ -1,20 +1,11 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 files {
-    'vehicles.meta',
-    'carvariations.meta',
-    'carcols.meta',
-    'handling.meta',
-    'vehiclelayouts.meta',    -- Not Required
+	'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carvariations.meta',
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
-
-
-client_script {
-    'vehicle_names.lua'    -- Not Required
-}
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
