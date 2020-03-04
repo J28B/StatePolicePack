6c72d98e65d75bee007b9325cb735ea5b2e6a43c
Jamie <jamiebeggs28@gmail.com>
2020-03-04 20:20:20 +0000

    CVPI Update

--- CVPI/__resource.lua
+++ CVPI/__resource.lua
@@ -1,13 +1,19 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Assets template by Albo1125.
+-- Look in the meta_templates folder for the default meta file entries when installing vehicles/peds into custom slots.
+-- You can (un)comment or add/remove files depending on which ones you use.
+-- Documentation of available data files for FiveM: https://wiki.fivem.net/wiki/Data_files
 
 files {
-	'data/vehicles.meta',
-	'data/carcols.meta',
-	'data/carvariations.meta',
+	'carcols.meta',
+        'vehicles.meta',
+	'carvariations.meta'
+
+	
 }
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
-
-client_script 'data/vehicle_names.lua'
+client_script 'vehicle_names.lua'
